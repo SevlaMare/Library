a85648bda8890506819211107efc46494b48f20f
Daniel Santander <danyvo1d@gmail.com>
2020-07-17 17:38:17 -0500

    Working with render function

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 const myLibrary = [];
 
 const collect_form_data = () => {
-  const author = document.querySelector("#author")
-  const title = document.querySelector("#title")
-  const pages = document.querySelector("#pages")
+  let author = document.getElementById("author").value
+  let title = document.getElementById("title").value
+  let pages = document.getElementById("pages").value
 
-  myLibrary.push( [author, title, pages] )
+  myLibrary.push( [ author, title, pages] )
 
-  
-  console.log( myLibrary[0][0] )
-};
+  render()
+
+}
+
 
 
 document.getElementById("btn-submit")
   .addEventListener("click", collect_form_data)
 
-  console.log("working")
+  function makeCard(myLibrary) {
+    let card = document.createElement("div");
+    card.className = "card";
+ 
+    card.textContent = `title: ${myLibrary[0]}`;
+    card.textContent = `author: ${myLibrary[1]}`;
+    card.textContent = `pages: ${myLibrary[2]}`;
+   
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+
+    return card;
+  }
+
+  function render() {
+    const booksContainer = document.getElementById('books-container');
+    booksContainer.innerHTML = '';
+      const card = document.createElement('div');
+  
+      card.id = `book-${idx}`;
+      card.className = 'card mt-3 mb-3 col-xs-12 col-sm-12 col-md-6 col-lg-3';
+      card.innerHTML = "<h5>Testing</h5>";
+      booksContainer.appendChild(card);
+  }
