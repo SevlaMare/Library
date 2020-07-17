227826148ade3f25ca8b894d037db0ef9ea6fc86
Mare <sevilamare@gmail.com>
2020-07-17 18:49:48 -0300

    draw v0

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const myLibrary = [];
 
+const collect_form_data = () => {
+  const author = document.querySelector("#author")
+  const title = document.querySelector("#title")
+  const pages = document.querySelector("#pages")
+
+  myLibrary.push( [author, title, pages] )
+
+  
+  console.log( myLibrary[0][0] )
+};
+
+
+document.getElementById("btn-submit")
+  .addEventListener("click", collect_form_data)
+
+  console.log("working")
