3d28e7d1de11fea385fb8ba6bf9099aa6918248b
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-07-21 10:07:24 -0500

    Snippet for appending elements.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 console.log("RUNNING E1");
 // const array1 = []
-
+document.getElementById("btn").addEventListener("click", get_input_fx);
 
 function get_input_fx() {
-  var x = document.getElementById("give-us-the-data").value
-  console.log(x)
+  var x = document.getElementById("give-us-the-data").value;
+  console.log(x);
+  let paragraph = document.createElement("p");
+  paragraph.innerHTML = "test";
+  paragraph.className = "card";
+  let div = document.getElementById("cards");
+  div.appendChild(paragraph);
 }
