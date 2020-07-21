60b9e6cb7231b7d2b0a337374b6ed95a02e8cbec
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-07-21 10:30:36 -0500

    Fix linter errors.

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-console.log("RUNNING E1");
-// const array1 = []
-document.getElementById("btn").addEventListener("click", get_input_fx);
-
-function get_input_fx() {
-  var x = document.getElementById("give-us-the-data").value;
-  console.log(x);
-  let paragraph = document.createElement("p");
-  paragraph.innerHTML = "test";
-  paragraph.className = "card";
-  let div = document.getElementById("cards");
+function getInputFx() {
+  const input = document.getElementById('give-us-the-data').value;
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = input;
+  paragraph.className = 'card';
+  const div = document.getElementById('cards');
   div.appendChild(paragraph);
 }
+
+document.getElementById('btn').addEventListener('click', getInputFx);
