5d2b3a5dc29173638615782dbdfa47b5ab925569
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-07-21 11:17:43 -0500

    Get book data and display it.

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-function getInputFx() {
-  const input = document.getElementById('give-us-the-data').value;
+const myLibrary = [];
+
+function createParagraph(text) {
   const paragraph = document.createElement('p');
-  paragraph.innerHTML = input;
-  paragraph.className = 'card';
-  const div = document.getElementById('cards');
-  div.appendChild(paragraph);
+  paragraph.innerHTML = text;
+  return paragraph;
 }
 
-document.getElementById('btn').addEventListener('click', getInputFx);
+function displayBooks() {
+  for (let index = 0; index < myLibrary.length; index += 1) {
+    const element = myLibrary[index];
+    const div = document.getElementById('cards');
+    div.appendChild(createParagraph(element[0]));
+    div.appendChild(createParagraph(element[1]));
+    div.appendChild(createParagraph(element[2]));
+  }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  const bookName = document.getElementById('bookName').value;
+  const bookAuthor = document.getElementById('bookAuthor').value;
+  const bookPages = document.getElementById('bookPages').value;
+  myLibrary.push([bookName, bookAuthor, bookPages]);
+  displayBooks();
+}
+
+document.getElementById('btnGetBookData').addEventListener('click', addBookToLibrary);
