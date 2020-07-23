767eb712cdf2d9a41260a1699f093592580f2b8a
Mare <sevilamare@gmail.com>
2020-07-23 19:51:30 -0300

    single book

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const myLibrary = [];
 const Book = (name, author, pages) => ({ name, author, pages });
 const getDataFromElement = idOrClass => document.querySelector(idOrClass).value;
 
+
 const createElement = (tag, text, className) => {
   const element = document.createElement(tag);
   element.innerHTML = text;
@@ -9,25 +10,37 @@ const createElement = (tag, text, className) => {
   return element;
 };
 
-const render = dictionary => {
-  const placeHere = document.querySelector("#shelf");
-  const { name, author, pages } = dictionary[0];
 
-  placeHere.appendChild(createElement('p', name, 'card'));
-  placeHere.appendChild(createElement('p', author, 'card'));
-  placeHere.appendChild(createElement('p', pages, 'card'));
+const render = (dictionary) => {
+  for (let count = 0; count < dictionary.length; count += 1) {
+    console.log(dictionary[count])
+
+    const shelf = document.querySelector('#shelf');
+    const { name, author, pages } = dictionary[count];
+
+    shelf.appendChild(createElement('p', name, 'card'));
+    shelf.appendChild(createElement('p', author, 'card'));
+    shelf.appendChild(createElement('p', pages, 'card'));
+  }
 };
+
 
 const addBookToLibrary = () => {
   myLibrary.push(
     Book(
       getDataFromElement('#bookName'),
       getDataFromElement('#bookAuthor'),
-      getDataFromElement('#bookPages'),
-    ),
+      getDataFromElement('#bookPages')
+    )
   );
   return render(myLibrary);
 };
+
+document
+  .querySelector('#btn-submit')
+  .addEventListener('click', addBookToLibrary);
+
+// TODO: export myLibrary to json
 
 // TODO: split render x renderAll
 // const renderNew = (dictionary) => {
@@ -35,9 +48,3 @@ const addBookToLibrary = () => {
 //     const element = myLibrary[count];
 //   }
 // }
-
-document
-  .querySelector('#btn-submit')
-  .addEventListener('click', addBookToLibrary);
-
-// TODO: export myLibrary to json
