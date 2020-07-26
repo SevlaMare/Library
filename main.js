941df732a7bfd9a53c83e23649f244cdb2fd2037
Mare <sevilamare@gmail.com>
2020-07-26 15:46:39 -0300

    render only new book

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 const Book = (name, author, pages) => ({ name, author, pages });
-const getDataFromElement = idOrClass => document.querySelector(idOrClass).value;
+const getValueFromElement = idOrClass => document.querySelector(idOrClass).value;
 
 
 const createElementX = (tag, className, text) => {
@@ -10,30 +10,34 @@ const createElementX = (tag, className, text) => {
   return element;
 };
 
+// render without loop
+const render = (book) => {
+  const shelf = document.querySelector('#shelf');
+  const { name, author, pages } = book;
 
-const render = (library) => {
-  for (let book = 0; book < library.length; book += 1) {
-    const shelf = document.querySelector('#shelf');
-    const { name, author, pages } = library[book];
-
-    const card = createElementX('div', 'card')
-    card.appendChild(createElementX('p', null, name));
-    card.appendChild(createElementX('p', null, author));
-    card.appendChild(createElementX('p', null, pages));
-    shelf.appendChild(card)
-  }
+  const card = createElementX('div', 'card')
+  card.appendChild(createElementX('p', null, name));
+  card.appendChild(createElementX('p', null, author));
+  card.appendChild(createElementX('p', null, pages));
+  shelf.appendChild(card)
 };
+
+// call on load, render all books thought loop using render()
+const renderAll = (library) => {
+  for (let book = 0; book < library.length; book += 1) {
+    const { name, author, pages } = library[book];
+  }
+}
 
 
 const addBookToLibrary = () => {
-  myLibrary.push(
-    Book(
-      getDataFromElement('#bookName'),
-      getDataFromElement('#bookAuthor'),
-      getDataFromElement('#bookPages')
+  book = Book(
+      getValueFromElement('#bookName'),
+      getValueFromElement('#bookAuthor'),
+      getValueFromElement('#bookPages')
     )
-  );
-  return render(myLibrary);
+  myLibrary.push(book);
+  return render(book);
 };
 
 
@@ -42,10 +46,3 @@ document
   .addEventListener('click', addBookToLibrary);
 
 // TODO: export myLibrary to json
-
-// TODO: split render x renderAll
-// const renderNew = (dictionary) => {
-//   for (let count = 0; count < dictionary.length; count += 1) {
-//     const element = myLibrary[count];
-//   }
-// }
